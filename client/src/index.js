8d98b1be38943654c0460cfe9403fc0fa665ebbb
kejto <lubos.kato@gmail.com>
2018-08-01 14:27:47 +0200

    optimize

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import { HashRouter } from 'react-router-dom'
 import App from './components/Start.jsx'
 import registerServiceWorker from './registerServiceWorker.js'
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 ReactDom.render((
     <HashRouter>
