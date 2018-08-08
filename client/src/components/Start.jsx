12e7fbd4e88aa448fd6c756413a6ae17a40d6a02
kejto <lubos.kato@gmail.com>
2018-08-08 16:12:56 +0200

    add prod url

--- client/src/components/Start.jsx
+++ client/src/components/Start.jsx
@@ -18,7 +18,7 @@ const store = createStore(rootReducer);
 const httpLink = new HttpLink({uri: 'http://localhost:3000/graphql'})
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:3001/subscriptions`,
+  uri: `ws://coindome.herokuapp.com/subscriptions`,
   options: {
     reconnect: true
   }
