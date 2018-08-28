5dc3214d96403298932fc6ae2a8c1826fceb90a4
kejto <lubos.kato@gmail.com>
2018-08-28 22:26:54 +0200

    test remove gzip

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -55,7 +55,7 @@ switch (process.env.NODE_ENV) {
           plugins.sw,
           plugins.copy,
           //plugins.uglifyJs,
-          plugins.Gzip
+          //plugins.Gzip
         ],
       }
 	  );
