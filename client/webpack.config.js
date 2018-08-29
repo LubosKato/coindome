48783add441bf6b2bf5b6358d3ea435c185e8c53
kejto <lubos.kato@gmail.com>
2018-08-29 10:41:05 +0200

    try minify

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,10 +45,34 @@ let config;
 switch (process.env.NODE_ENV) {
   case 'production':
     config = merge(
-	    common,
+      common,
       {
         devtool: 'source-map',
         mode: 'production',
+        optimization: {
+          minimize: true,
+          runtimeChunk: true,
+          noEmitOnErrors: true,
+          splitChunks: {
+            chunks: 'async',
+            minSize: 1000,
+            minChunks: 2,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            name: true,
+            cacheGroups: {
+              default: {
+                minChunks: 1,
+                priority: -20,
+                reuseExistingChunk: true,
+              },
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10,
+              },
+            },
+          },
+        },
         plugins: [
           plugins.loaderOptions,
           plugins.environmentVariables,
@@ -65,7 +89,7 @@ switch (process.env.NODE_ENV) {
           }),
         ],
       },
-	  );
+    );
     break;
   case 'development':
     config = merge(
