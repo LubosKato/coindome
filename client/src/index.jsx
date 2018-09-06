0b9267b7f6124c70fd6a170d41a7f041dbf7b20d
kejto <lubos.kato@gmail.com>
2018-09-06 10:46:39 +0200

    move bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import App from './components/Start';
-import registerServiceWorker from './registerServiceWorker';
 import 'style-loader!react-select/scss/default.scss';
 import '!style-loader!css-loader!react-toastify/dist/ReactToastify.css';
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDom.render((
   <HashRouter>
@@ -15,5 +14,5 @@ ReactDom.render((
 ), document.getElementById('react-app'));
 
 if ('serviceWorker' in navigator) {
-  const registration = runtime.register();
+  runtime.register();
 }
