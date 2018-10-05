e3e2960fd57382c4e0e5b6ac7f3c2f54e3eb0931
kejto <lubos.kato@gmail.com>
2018-10-05 15:53:48 +0200

    update var

--- server/utils/other.utils.js
+++ server/utils/other.utils.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 module.exports = {
   sendMail: function(req, res, user, token) { 
           // Send the email
-          var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: 'kejto', pass: process.env.SENDGRID } });
+          var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: 'kejto', pass: process.env.sg } });
           var mailOptions = { from: 'no-reply@coindome.com', to: user.email, subject: t('tokenSubject'), text: 'Hello,\n\n' + t('verifyContent') +' \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
           transporter.sendMail(mailOptions, function (err) {
             if (err) { return res.status(500).send({ success: false, message: err.message }); }
